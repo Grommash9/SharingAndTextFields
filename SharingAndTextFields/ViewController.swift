a1ae5f0cde2ca6cb8438a3e972d4a4f044675ae8
Alexandr <prudnikov21@icloud.com>
2020-06-27 11:34:43 -0700

    Initial Commit

---
+++ SharingAndTextFields/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SharingAndTextFields
+//
+//  Created by Александр on 27.06.2020.
+//  Copyright © 2020 Александр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
